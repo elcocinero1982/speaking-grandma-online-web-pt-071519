a4604d092f5c95a2822941d2a6bb41b4c2fe8856
jose luis <cortezluisjose@gmail.com>
2019-07-21 01:54:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,7 +12,7 @@ elsif
 puts "I LOVE YOU GRANDMA!"
 # NO, NOT SINCE 1938!
 else   
-  
+  puts 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
