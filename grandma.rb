7fa2d2abb819ccb1be25c18c2077a1db65784f2a
jose luis <cortezluisjose@gmail.com>
2019-07-21 02:14:46 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,10 +4,10 @@
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if  
+elsif  "you shout, she can hear you (or at least she thinks so"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
-
+puts 
 # NO, NOT SINCE 1938!
 
 
