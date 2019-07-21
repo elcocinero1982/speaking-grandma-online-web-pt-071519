e71116062c059147549e05e95e8977d5b8fcb43c
jose luis <cortezluisjose@gmail.com>
2019-07-21 01:54:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,9 +9,10 @@ puts NO, NOT SINCE 1938!
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 elsif
-puts 
+puts "I LOVE YOU GRANDMA!"
 # NO, NOT SINCE 1938!
-
+else   
+  
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
