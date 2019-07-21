e255538c5efbaf83093bce3812629c9e50d73bcf
jose luis <cortezluisjose@gmail.com>
2019-07-21 01:49:10 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,8 @@ def speak_to_grandma
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-
+If
+""
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 
