3ef4f3f3f5201844ebb03c3ec010dc143b7a7417
jose luis <cortezluisjose@gmail.com>
2019-07-21 01:58:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 If
-"HUH?! SPEAK UP, SONNY!".capitals
+speak_to_grandma.capitals
 puts NO, NOT SINCE 1938!
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
